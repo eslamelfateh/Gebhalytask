6c94338318b4b16da565f95a4391f9249272e24e
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2021-06-20 00:52:28 +0200

    Updates

---
+++ precache-manifest.7841bf6730d252d5eb567f0488482bf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf475924d8f94915d30fe90c68b5f272",
+    "url": "/Gebhalytask/index.html"
+  },
+  {
+    "revision": "8391066470929353f293",
+    "url": "/Gebhalytask/static/css/main.7de7441b.chunk.css"
+  },
+  {
+    "revision": "b38871d9d73b6cc531f6",
+    "url": "/Gebhalytask/static/js/2.a57d814f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Gebhalytask/static/js/2.a57d814f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8391066470929353f293",
+    "url": "/Gebhalytask/static/js/main.9422313d.chunk.js"
+  },
+  {
+    "revision": "20f81a531d130b4ef060",
+    "url": "/Gebhalytask/static/js/runtime-main.f2e34c7e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Gebhalytask/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Gebhalytask/static/media/shopping-bag.30f07071.svg"
+  }
+]);
