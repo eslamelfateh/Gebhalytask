9d1e1ed39c7ce9c640438b5d698e53bbbf28f7da
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2021-06-20 00:27:44 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route , Switch } from 'react-router-dom';
+
+import './App.css';
+import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
+
+
+
+class App extends React.Component {
+  render(){  
+    return (
+      <div>
+        <Header />  
+        <Switch>
+          <Route  exact path='/' component={ShopPage}/>
+          <Route path='/checkout' component={CheckoutPage}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux'; 
+import { createStructuredSelector } from 'reselect';
+
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
+
+import {ReactComponent as Logo } from '../../assets/crown.svg'
+
+import './header.styles.scss';
+
+const Header = ({currentUser,hidden}) => {
+  return (
+    <div className='header'>
+      <Link className='logo-container' to="/">
+        <Logo className='logo'/>
+      </Link>
+      <div className="options">
+        <Link className="option" to='/'>
+          SHOP
+        </Link>
+        <CartIcon/>
+      </div>
+      {
+        hidden ? null : <CartDropdown/>
+      }
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  hidden: selectCartHidden
+})
+
+export default  connect(mapStateToProps)(Header);
